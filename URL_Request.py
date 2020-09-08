bfff06946ea849c2367d0cf06e932511cb7d0e7b
NickFreedman25 <69538037+NickFreedman25@users.noreply.github.com>
2020-09-08 13:15:04 -0700

    Soup Branch
    Working commit using Soup/Pandas. Allows extraction of job titles.

--- URL_Request.py
+++ URL_Request.py
@@ -5,13 +5,25 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import time
 
-def get_URL_data():
-    URL = "https://www.indeed.com/jobs?q=Software+Engineer&explvl=entry_level"
-    #Request of the stated URL Above
-    page = requests.get(URL)
+class URL_Request:
+    def __init__(self):
+        self.data = self.get_URL_data()
+        
+    def get_URL_data(self):
+        URL = "https://www.indeed.com/jobs?q=Software+Engineer&explvl=entry_level"
+        #Request of the stated URL Above
+        page = requests.get(URL)
 
-    soup = BeautifulSoup(page.text,"html.parser")
+        soup = BeautifulSoup(page.text,"html.parser")
+        
+        return soup
 
-    print(soup.prettify())
+    def extract_data(self):
+        jobs = []
+        for div in self.data.find_all(name="div", attrs={"class":"row"}):
+            for a in div.find_all(name="a", attrs={"data-tn-element":"jobTitle"}):
+                jobs.append(a["title"])
+        print(jobs)
 
-get_URL_data()
+data = URL_Request()
+data.extract_data()    
